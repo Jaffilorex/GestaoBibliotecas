a6ce050529cc6937fc1cfe5c0e2a64fac8a57a26
Jaffilorex <jabato.00007@gmail.com>
2024-11-12 17:55:53 +0100

    Update Multa.h

--- Multa.h
+++ Multa.h
@@ -1,20 +1,34 @@
+// Multa.h
 #ifndef MULTA_H
 #define MULTA_H
 
-#include "Pessoa.h"
+#include <iostream>
+#include "Leitor.h"
 
+class Multa {
+private:
+    int diasAtraso;
+    Leitor* leitor;
 
-class Multa
-{
-    public:
-        Multa();
-        virtual ~Multa();
+public:
+    Multa(int diasAtraso, Leitor* leitor) : diasAtraso(diasAtraso), leitor(leitor) {}
 
-    protected:
+    double calcularValor() const {
+        double taxaBase = 2.0;  // Base rate per day
 
-    private:
-        int dias_atraso;
-        Leitor *leitor;
+        if (auto estudante = dynamic_cast<Estudante*>(leitor)) {
+            return diasAtraso * taxaBase * 0.5;  // 50% discount for students
+        } else if (auto professor = dynamic_cast<Professor*>(leitor)) {
+            return diasAtraso * taxaBase * 0.7;  // 30% discount for professors
+        } else {
+            return diasAtraso * taxaBase;  // No discount for other readers
+        }
+    }
+
+    void mostrarMulta() const {
+        std::cout << "Multa para " << leitor->detalhes()
+                  << ": " << calcularValor() << " unidades.\n";
+    }
 };
 
 #endif // MULTA_H
